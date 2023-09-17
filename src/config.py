fd4e583513318050b3a28a84285f863c41044356
Changjin Lee <changjinlee@Changjins-MacBook-Pro.local>
2023-09-17 21:54:07 +0900

    4-layered architecture baseline

--- src/config.py
+++ src/config.py
@@ -1,22 +1,52 @@
-from functools import lru_cache
+import os
+from dataclasses import dataclass
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
-
-load_dotenv(".env")
 
 
-class DBSettings(BaseSettings):
+@dataclass
+class DBConfig:
     MYSQL_HOST: str
     MYSQL_DB_NAME: str
     MYSQL_USERNAME: str
     MYSQL_PASSWORD: str
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+
+@dataclass
+class JWTConfig:
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
 
 
-@lru_cache
-def get_db_settings():
-    return DBSettings()
+@dataclass
+class MailgunConfig:
+    DOMAIN: str
+    API_KEY: str
+
+
+@dataclass
+class Config:
+    db: DBConfig
+    jwt: JWTConfig
+    mailgun: MailgunConfig
+
+
+def load_config() -> Config:
+    load_dotenv()
+    return Config(
+        db=DBConfig(
+            MYSQL_HOST=os.environ["MYSQL_HOST"],
+            MYSQL_DB_NAME=os.environ["MYSQL_DB_NAME"],
+            MYSQL_USERNAME=os.environ["MYSQL_USERNAME"],
+            MYSQL_PASSWORD=os.environ["MYSQL_PASSWORD"],
+        ),
+        jwt=JWTConfig(
+            SECRET_KEY=os.environ["JWT_SECRET_KEY"],
+            ALGORITHM=os.environ["JWT_ALGORITHM"],
+            ACCESS_TOKEN_EXPIRE_MINUTES=int(os.environ["JWT_ACCESS_TOKEN_EXPIRE_MINUTES"]),
+            REFRESH_TOKEN_EXPIRE_MINUTES=int(os.environ["JWT_REFRESH_TOKEN_EXPIRE_MINUTES"]),
+        ),
+        mailgun=MailgunConfig(DOMAIN=os.environ["MAILGUN_DOMAIN"], API_KEY=os.environ["MAILGUN_API_KEY"]),
+    )
--- src/main.py
+++ src/main.py
@@ -1,29 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-import src.user.models as user_models
-from src.auth.router import router as auth_router
-from src.category.router import router as category_router
-from src.database import engine
-from src.task.router import router as task_router
-
-user_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(auth_router)
-app.include_router(category_router)
-app.include_router(task_router)
-
 
 @app.get("/")
-async def root():
-    return {"message": "hello to the root"}
+def health_check():
+    return {"message": "I'm doing fine, thanks for asking"}
